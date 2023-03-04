16df348fa017a5059daa9a5ec957eb728c6fc800
boo <eyurchenko@specialist.ru>
2023-03-04 18:28:10 +0700

    add deserialize schema + author surname

--- api/handlers/author.py
+++ api/handlers/author.py
@@ -2,6 +2,7 @@ from api import app, db, request
 from api.models.quote import QuoteModel
 from api.models.author import AuthorModel
 from api.schemas.author import author_schema, authors_schema
+from marshmallow import ValidationError
 
 
 # Сериализация:
@@ -24,8 +25,13 @@ def get_author_by_id(author_id):
 
 @app.route('/authors', methods=["POST"])
 def create_author():
-    author_data = request.json
-    author = AuthorModel(author_data["name"])
+    json_data = request.json
+    try:
+        author_data = author_schema.load(json_data)
+    except ValidationError as err:
+        return err.messages, 422
+
+    author = AuthorModel(**author_data)
     db.session.add(author)
     db.session.commit()
     return author_schema.dump(author), 201
--- api/models/author.py
+++ api/models/author.py
@@ -4,13 +4,15 @@ from api import db
 class AuthorModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
+    surname = db.Column(db.String(32), nullable=False, server_default="Иванов")
     quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
 
-    def __init__(self, name):
-        self.name = name
+    # def __init__(self, name):
+    #     self.name = name
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name
-        }
+
+    # def to_dict(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name
+    #     }
